6c0799c384b3e8d77b4bc6a54bf84ba283d04798
Evgeny Ivanov <busforward@gmail.com>
2016-05-30 03:33:05 +0500

    Add cart and catalog page

--- js/function.js
+++ js/function.js
@@ -64,4 +64,88 @@ $(document).ready(function() {
 
 	});
 
+	// Catalog search view more
+	$('.catalog-head-list__opener').click(function() {
+		var self = $(this);
+		var listEl = $(this).parent('.catalog-head-list').find('.catalog-head-list__item');
+		var moreBtnClose = $(this).hasClass('opened');
+		var moreBtnTxt = $('.catalog-head-list__opener > span');
+
+		self.toggleClass('opened');
+		$(listEl).toggleClass('hidden_off');
+
+		if (moreBtnClose == true) {
+			moreBtnTxt.html('Показать всё');
+		} else {
+			moreBtnTxt.html('Скрыть');
+		}
+
+	});
+
+	// Filter row change arrow
+	$('.catalog-sort__sort a').click(function() {
+		$('.dotted-element--arrow').toggleClass('up');
+	});
+
+	// Formstyler
+	$('select').chosen();
+
+	// UI Price
+	$("#slider").slider({
+		min: 0,
+		max: 7000,
+		values: [0,7000],
+		range: true,
+		stop: function(event, ui) {
+			jQuery("input#minCost").val(jQuery("#slider").slider("values",0));
+			jQuery("input#maxCost").val(jQuery("#slider").slider("values",1));
+	    },
+	    slide: function(event, ui){
+			jQuery("input#minCost").val(jQuery("#slider").slider("values",0));
+			jQuery("input#maxCost").val(jQuery("#slider").slider("values",1));
+	    }
+	});
+
+
+	// Hide/show more fillter info
+	$('.more-items-toggler').click(function() {
+		$(this).closest('.filter-wrap').find('.hidden-filters').toggleClass('hidden');
+	});
+
+	// Hide/show more address
+	$('.skill-more').click(function() {
+		$(this).parent().find('.hidden-toggle').toggleClass('hidden');
+		$(this).toggleClass('opened');
+
+		var self = $(this);
+		var openedLink = $(this).hasClass('opened');
+
+		if (openedLink == true) {
+			self.html('Скрыть');
+		} else {
+			self.html('Показать ещё');
+		}
+
+	});
+
+	// Chose teacher
+	$('.teacher-card-button .btn').click(function() {
+		$(this).toggleClass('btn-green').find('span').toggleClass('ng-hide');
+	});
+
+	// Open/close more info teacher
+	$('.teacher-card-info__links .dotted-element').click(function() {
+		$(this).closest('.teacher-card-info__openable').toggleClass('closed');
+		$(this).closest('.teacher-card-info__openable').toggleClass('opened');
+
+		var thisWrap = $(this).closest('.teacher-card-info__openable').hasClass('closed');
+
+		if (thisWrap == true) {
+			$(this).html('Развернуть');
+		} else {
+			$(this).html('Свернуть');
+		}
+
+	});
+
 });
